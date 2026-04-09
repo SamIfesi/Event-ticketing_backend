18eaf577b6c4e46a5c1f00083c5b679cee0ce4dc
SamIfesi <Ekesamrose@gmail.com>
2026-04-09 15:31:18 +0100

    Refactor QRCodeService to use BaconQrCode library for SVG generation and improve file handling

--- services/QRCodeService.php
+++ services/QRCodeService.php
@@ -1,33 +1,47 @@
 <?php
-class QRCOdeService {
-  public static function generate(string $qrToken): string {
-    require_once __DIR__ . '/../libs/phpqrcode/qrlib.php';
 
+use BaconQrCode\Renderer\Image\ImagickImageBackend;
+use BaconQrCode\Renderer\Image\SvgImageBackend;
+use BaconQrCode\Renderer\ImageRenderer;
+use BaconQrCode\Renderer\RendererStyle\RendererStyle;
+use BaconQrCode\Writer;
+
+class QRCOdeService
+{
+  public static function generate(string $qrToken): string
+  {
     $outputDir = Constants::STORAGE_QRCODES;
-    $filePath  = $outputDir . $qrToken . '.png';
+    $filePath  = $outputDir . $qrToken . '.svg';
 
     if (file_exists($filePath)) {
       return $filePath;
     }
 
-    if(!is_dir($outputDir)) {
+    if (!is_dir($outputDir)) {
       mkdir($outputDir, 0755, true);
     }
+    $renderer = new ImageRenderer(
+      new RendererStyle(300),
+      new SvgImageBackend()
+    );
 
-    QRcode::png($qrToken, $filePath, QR_ECLEVEL_M, 8, 2);
+    $writer = new Writer($renderer);
+    $writer->writeFile($qrToken, $filePath);
 
     return $filePath;
   }
 
-  public static function getUrl(string $qrToken): string{
-    $appUrl = Environment::get('APP_URL', 'http://localhost/');
-    return "{$appUrl}/storage/qrcodes/{$qrToken}.png";
+  public static function getUrl(string $qrToken): string
+  {
+    $appUrl = Environment::get('APP_URL', 'http://localhost');
+    return "{$appUrl}/storage/qrcodes/{$qrToken}.svg";
   }
 
-  public static function delete(string $qrToken): void{
-    $filePath = Constants::STORAGE_QRCODES . $qrToken . '.png';
+  public static function delete(string $qrToken): void
+  {
+    $filePath = Constants::STORAGE_QRCODES . $qrToken . '.svg';
 
-    if (file_exists($filePath)){
+    if (file_exists($filePath)) {
       unlink($filePath);
     }
   }
