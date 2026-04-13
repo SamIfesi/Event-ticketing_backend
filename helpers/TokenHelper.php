0056d0df752d1a3aeba6a28ce35921dcd79e89fa
SamIfesi <Ekesamrose@gmail.com>
2026-04-13 11:49:31 +0100

    Fix string interpolation in token generation methods in TokenHelper

--- helpers/TokenHelper.php
+++ helpers/TokenHelper.php
@@ -3,12 +3,12 @@ class TokenHelper{
   public static function generateQRToken():string{
     $timestamp = time();
     $random    = strtoupper(substr(bin2hex(random_bytes(4)), 0, 6));
-    return 'TKT-{$timestamp}-{$random}';
+    return "TKT-{$timestamp}-{$random}";
     }
     
     public static function generatePaystackReference(): string {
       $timestamp = time();
       $random    = strtoupper(substr(bin2hex(random_bytes(4)), 0, 6));
-      return 'EVT-{$timestamp}-{$random}';
+      return "EVT-{$timestamp}-{$random}";
   }
 }
