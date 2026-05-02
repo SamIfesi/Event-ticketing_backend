f2922b0f566f9c499d816e3ee941865de1edbcac
SamIfesi <Ekesamrose@gmail.com>
2026-05-02 21:41:31 +0100

    Add routes for organizer applications with attendee and admin functionalities

---
+++ routes/organizer_applications.php
@@ -0,0 +1,10 @@
+<?php
+
+//Attendee routes
+$router->post('/api/org_applications', [OrganizerApplicationController::class, 'store'],  [AuthMiddleware::class]);
+$router->post('/api/org_applications/mine', [OrganizerApplicationController::class, 'mine'],  [AuthMiddleware::class]);
+
+// Admin routes
+$router->get('/api/admin/org_applications', [OrganizerApplicationController::class, 'index'],  [AuthMiddleware::class, RoleMiddleware::class => ['admin', 'dev']]);
+$router->put('/api/admin/org_applications/:id/approve', [OrganizerApplicationController::class, 'approve'],  [AuthMiddleware::class, RoleMiddleware::class => ['admin', 'dev']]);
+$router->put('/api/admin/org_applications/:id/reject', [OrganizerApplicationController::class, 'reject'],  [AuthMiddleware::class, RoleMiddleware::class => ['admin', 'dev']]);
