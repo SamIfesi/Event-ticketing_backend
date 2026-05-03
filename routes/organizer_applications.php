3d63a345c60a2e20da19a2edec0035bc53600ef0
SamIfesi <Ekesamrose@gmail.com>
2026-05-03 21:47:37 +0100

    Fix route method for retrieving user's organizer applications

--- routes/organizer_applications.php
+++ routes/organizer_applications.php
@@ -2,7 +2,7 @@
 
 //Attendee routes
 $router->post('/api/org_applications', [OrganizerApplicationController::class, 'store'],  [AuthMiddleware::class]);
-$router->post('/api/org_applications/mine', [OrganizerApplicationController::class, 'mine'],  [AuthMiddleware::class]);
+$router->get('/api/org_applications/mine', [OrganizerApplicationController::class, 'mine'],  [AuthMiddleware::class]);
 
 // Admin routes
 $router->get('/api/admin/org_applications', [OrganizerApplicationController::class, 'index'],  [AuthMiddleware::class, RoleMiddleware::class => ['admin', 'dev']]);
